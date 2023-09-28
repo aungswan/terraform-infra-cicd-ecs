cb8f7c65784df58a1ecde3b9109d4702c4881423
Aung Swan <uaungswan@gmail.com>
2023-09-28 14:35:56 +0800

    deployed iac

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,9 +1,9 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = 
-    key            = 
-    region         = 
-    dynamodb_table = 
+    bucket         = "ecommerce-web01-tf-state"
+    key            = "asfreeze-app/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "terraform-state-lock"
   }
 }
